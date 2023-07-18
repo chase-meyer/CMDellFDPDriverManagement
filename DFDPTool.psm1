bba320bd5657cf6d77478bdc45beb8071886beb8
Chase Meyer <chase.e.meyer@gmail.com>
2023-07-18 07:15:52 -0500

    add DFDPTool.psm1

---
+++ DFDPTool.psm1
@@ -0,0 +1,3 @@
+# TODO: get functions into a module 
+#    * https://docs.microsoft.com/en-us/powershell/scripting/developer/module/how-to-write-a-powershell-script-module?view=powershell-7.1
+
--- DownloadScript.ps1
+++ DownloadScript.ps1
@@ -7,14 +7,14 @@ $HTML = Invoke-RestMethod "https://www.dell.com/support/kbdoc/en-us/000180534/de
 $CellPattern = '<td colspan="1" rowspan="1">(?<CellData>.*)</td>'
 
 $AllMatches = $HTML | 
-    Select-String $CellPattern -AllMatches | 
-        Select-Object -ExpandProperty Matches
+Select-String $CellPattern -AllMatches | 
+Select-Object -ExpandProperty Matches
 
 $Table = @()
 
 $Row = [PSCustomObject]@{
-    'Family' = $null
-    'Models' = $null
+    'Family'     = $null
+    'Models'     = $null
     'Win11_22H2' = $null
     'Win11_21H2' = $null
     'Win10_22H2' = $null
@@ -24,45 +24,44 @@ $Row = [PSCustomObject]@{
 
 $i = 1
 
-foreach ($Cell in $AllMatches)
-{
+foreach ($Cell in $AllMatches) {
     $i++
     Switch ($i % 8) {
         1 {
-            $Row.'Family' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value
+            $Row.'Family' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value
         }
         2 {
-            $Row.'Models' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value
+            $Row.'Models' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value
         }
         3 {
-            $Row.'Win11_22H2' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value        
+            $Row.'Win11_22H2' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value        
             $Row.'Win11_22H2' = $Row.'Win11_22H2' -replace '.+a href="(.+?)".+', '$1'
         }
         4 {
-            $Row.'Win11_21H2' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value        
+            $Row.'Win11_21H2' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value        
             $Row.'Win11_21H2' = $Row.'Win11_21H2' -replace '.+a href="(.+?)".+', '$1'
         }
         5 {
-            $Row.'Win10_22H2' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value        
+            $Row.'Win10_22H2' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value        
             $Row.'Win10_22H2' = $Row.'Win10_22H2' -replace '.+a href="(.+?)".+', '$1'
         }
         6 {
-            $Row.'Win10_21H2' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value
+            $Row.'Win10_21H2' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value
             $Row.'Win10_21H2' = $Row.'Win10_21H2' -replace '.+a href="(.+?)".+', '$1'
         }
         7 {
-            $Row.'Win10_21H1' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value        
+            $Row.'Win10_21H1' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value        
             $Row.'Win10_21H1' = $Row.'Win10_21H1' -replace '.+a href="(.+?)".+', '$1'
         }
         0 {
-            $Row.'Win10_20H2' = $Cell.Groups.Where{$_.Name -like 'CellData'}.Value
+            $Row.'Win10_20H2' = $Cell.Groups.Where{ $_.Name -like 'CellData' }.Value
             $Row.'Win10_20H2' = $Row.'Win10_20H2' -replace '.+a href="(.+?)".+', '$1'
 
             $Table += $Row
 
             $Row = [PSCustomObject]@{
-                'Family' = $null
-                'Models' = $null
+                'Family'     = $null
+                'Models'     = $null
                 'Win11_22H2' = $null
                 'Win11_21H2' = $null
                 'Win10_22H2' = $null
@@ -92,8 +91,8 @@ foreach ($Cell in $AllMatches)
 # }
 
 $Src = $Table | 
-    Where-Object {$_.Models -like "*7230*"} | 
-        Select-Object Win10_21H2 
+Where-Object { $_.Models -like "*7230*" } | 
+Select-Object Win10_21H2 
 
 $Dest = ""
 $Dest = $Dest + $Src | Split-Path -Leaf
